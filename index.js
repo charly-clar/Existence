a026f5cfc9a62e5fe2b00a1aef1b5d1bdeb7cfe5
charly clar <charlyclar.pro@gmail.com>
2021-12-30 22:33:24 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2022", "Genesis block", "0");
+    }
+
+    getLatesBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatesBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+      
+            if (previousBlock.hash !== currentBlock.previousHash) {
+              return false;
+            }
+      
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+              return false;
+            }
+          }
+      
+          return true;
+    }
+}
+
+let savjeeCoin = new Blockchain();
+
+console.log('Mining block 1....');
+savjeeCoin.addBlock(new Block(1, "10/07/2022", { amount: 4 }));
+
+console.log('Mining block 2....');
+savjeeCoin.addBlock(new Block(2, "12/07/2022", { amount: 10 }))
+ 
